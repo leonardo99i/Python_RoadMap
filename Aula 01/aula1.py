caed40146e220bbdfc46e4116f7dd3179734b9da
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-10-06 00:08:04 -0300

    Iniciando

---
+++ Aula 01/aula1.py
@@ -0,0 +1,11 @@
+#Para criar um comentário em python
+
+print('Linha 1')
+print('Linha 2')
+#Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+print('Linha 6')
+
+
