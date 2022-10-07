0272652439f44c95e4df6b2f2f330574a45e1e30
Leonardo R. Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-10-07 09:40:03 -0300

    Ajustando a função

--- Aula 01/aula1.py
+++ Aula 01/aula1.py
@@ -4,7 +4,8 @@ def bubble_sort(data):
   lst = list(data)
   for i in range(len(lst)):
     for j in range(i+1, len(lst)):
-      if lst[j], lst[i] = lst[i], lst[j]
+        if lst[j] < lst[i]:
+          lst[j], lst[i] = lst[i], lst[j]
   return lst
 
 itens = [4,1,5,3,2]
